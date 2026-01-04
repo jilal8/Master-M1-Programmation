9217012e02e241abe75034c42d99c0cffac1b158
jilal8 <mahdi.djilal@dct.dj>
2026-01-04 19:30:30 +0300

    création des models, Personne et Etudiant, il reste leur implemenation

---
+++ poo/src/main/java/com/tp/poo/model/Etudiant.java
@@ -0,0 +1,7 @@
+package com.tp.poo.model;
+
+public class Etudiant {
+	/**
+	 * TODO implementer ici les information de la classe Etudiant et implementer aussi l'heritage.
+	 */
+}
---
+++ poo/src/main/java/com/tp/poo/model/Personne.java
@@ -0,0 +1,7 @@
+package com.tp.poo.model;
+
+public class Personne {
+/**
+ * TODO implementer ici les information de la classe Personne.
+ */
+}
