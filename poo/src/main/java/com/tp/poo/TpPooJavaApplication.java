afc3ba57bee7cf1db26bc292676bf868e7c59c79
jilal8 <mahdi.djilal@dct.dj>
2026-01-04 19:25:58 +0300

    initialisation du premier TP

---
+++ poo/src/main/java/com/tp/poo/TpPooJavaApplication.java
@@ -0,0 +1,13 @@
+package com.tp.poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpPooJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpPooJavaApplication.class, args);
+	}
+
+}
---
+++ poo/src/test/java/com/tp/poo/TpPooJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpPooJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
